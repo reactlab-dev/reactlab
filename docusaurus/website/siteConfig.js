92ed89bc9b9493bf411087dfaa8f8669e6f50b8c
Thierry ABALEA <t.abalea@gmail.com>
2019-04-13 13:48:38 +0200

    configure docusaurus to deploy on gh-pages

--- docusaurus/website/siteConfig.js
+++ docusaurus/website/siteConfig.js
@@ -21,17 +21,17 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
-  tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  title: 'React Lab', // Title for your website.
+  tagline: 'Learn React by implementing a webapp accompanied by slides',
+  url: 'https://reactlab-dev.github.io', // Your website URL
+  baseUrl: '/reactlab/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'facebook',
+  projectName: 'reactlab',
+  organizationName: 'reactlab-dev',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'

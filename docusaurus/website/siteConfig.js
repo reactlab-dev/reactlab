e65c2049f7c5251918e475c5a263301294e54268
Thierry ABALEA <t.abalea@gmail.com>
2019-04-13 15:31:28 +0200

    try #1 to fix docusaurus dir path

--- docusaurus/website/siteConfig.js
+++ docusaurus/website/siteConfig.js
@@ -31,7 +31,7 @@ const siteConfig = {
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'reactlab',
+  projectName: '',
   organizationName: 'reactlab-dev',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...

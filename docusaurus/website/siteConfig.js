a88249aa8ffceaa9f5e2997370f0425e8145af63
Thierry ABALEA <t.abalea@gmail.com>
2019-04-13 15:25:12 +0200

    add a cname in docusaurus config

--- docusaurus/website/siteConfig.js
+++ docusaurus/website/siteConfig.js
@@ -24,6 +24,7 @@ const siteConfig = {
   title: 'React Lab', // Title for your website.
   tagline: 'Learn React by implementing a webapp accompanied by slides',
   url: 'https://reactlab.dev', // Your website URL
+  cname: 'reactlab.dev',
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',

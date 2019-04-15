e014681a6b431a01c8ead994915db6fc2132c990
Thierry ABALEA <t.abalea@gmail.com>
2019-04-15 08:03:01 +0200

    remove test deploy trigger content addition

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -77,7 +77,7 @@ class HomeSplash extends React.Component {
           <ProjectTitle />
           <PromoSection>
             <Button href={docUrl('lab-intro', language)}>
-              C'est parti ! Let's go !
+              C'est parti !
             </Button>
           </PromoSection>
         </div>

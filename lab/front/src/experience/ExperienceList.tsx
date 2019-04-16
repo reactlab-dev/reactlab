c90bb13756759e52c1834771701264cfc820209c
Eliott Balette <eliott.balette@gmail.com>
2019-04-16 21:54:46 +0200

    start here

--- lab/front/src/experience/ExperienceList.tsx
+++ lab/front/src/experience/ExperienceList.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
-// import styles from './ExperienceList.module.css';
-// import { Experience } from '../model';
+import styles from './ExperienceList.module.css';
+import { Experience } from '../model';
+
+const experiences: Experience[] = require('../data/experiences.json');
 
 function ExperienceList() {
-  return <div>TODO IMPLEMENT ME</div>;
+  return (
+    <div className={styles['list-main-container']}>
+      <div className={styles['list-container']}>
+        {experiences.map(experience => (
+          <ExperienceCard experience={experience} key={experience.id} />
+        ))}
+      </div>
+    </div>
+  );
 }
+
+const ExperienceCard = ({
+  experience: { name, organisation, expertise },
+}: {
+  experience: Experience;
+}) => (
+  <div className={styles['experience-card']}>
+    <h5 className={styles['name']}>{name}</h5>
+    <p className={styles['expertise']}>{expertise}</p>
+    <p className={styles['organisation-label']}>Team organisation</p>
+    <p className={styles['text']}>{organisation}</p>
+    <p className={styles['more-about']}>More about</p>
+  </div>
+);
 
 export default ExperienceList;

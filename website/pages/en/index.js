0039071d43d335dd93216caa5a51712991b36f60
Thierry ABALEA <t.abalea@gmail.com>
2019-04-17 11:05:47 +0200

    fix 'c'est parti' button on homepage

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -76,7 +76,7 @@ class HomeSplash extends React.Component {
         <div className="inner">
           <ProjectTitle />
           <PromoSection>
-            <Button href={docUrl('lab-into', language)}>C'est parti !</Button>
+            <Button href={docUrl('lab-intro', language)}>C'est parti !</Button>
           </PromoSection>
         </div>
       </SplashContainer>

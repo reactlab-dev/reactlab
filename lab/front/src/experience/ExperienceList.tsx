e31a8870ec8b517e9ff0cd6a632df10ab1bb903e
Eliott Balette <eliott.balette@gmail.com>
2019-04-17 08:59:07 +0200

    start here

--- lab/front/src/experience/ExperienceList.tsx
+++ lab/front/src/experience/ExperienceList.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import styles from './ExperienceList.module.css';
 import { Experience } from '../model';
 import Details from './ExperienceDetails';
+const { Provider, Consumer } = React.createContext<DataProviderContext>({
+  experiences: [],
+  onFilterChange: () => {},
+});
 
 async function fetchExperiences(filter?: string): Promise<Experience[]> {
   const result = await fetch(
@@ -19,88 +23,109 @@ class ExperienceList extends React.Component<{}, { filter?: string }> {
 
   render() {
     return (
-      <div className={styles['list-main-container']}>
-        <div className={styles['filter-container']}>
-          <input
-            className={styles['filter-input']}
-            onChange={async ({ target: { value: filter } }) => {
-              this.setState({ filter });
-            }}
-          />
+      <DataProvider>
+        <div className={styles['list-main-container']}>
+          <Consumer>
+            {(context: DataProviderContext) => (
+              <div className={styles['filter-container']}>
+                <input
+                  className={styles['filter-input']}
+                  onChange={async ({ target: { value: filter } }) => {
+                    context.onFilterChange(filter);
+                  }}
+                />
+              </div>
+            )}
+          </Consumer>
+          <FilteredExperienceList />
         </div>
-        <ConnectedList filter={this.state.filter} />
-      </div>
+      </DataProvider>
     );
   }
 }
 
-function connectDataProvider(
-  Component: React.ComponentType<{ experiences: Experience[] }>,
-): React.ComponentClass<{ filter?: string }> {
-  return class extends React.Component<
-    { filter?: string },
-    { experiences: Experience[] }
-  > {
-    constructor(props: { filter?: string }) {
-      super(props);
-      this.state = {
-        experiences: [],
-      };
-    }
+interface DataProviderContext {
+  experiences: Experience[];
+  onFilterChange: (filter: string) => void;
+}
 
-    async fetchExperiences() {
-      const experiences = await fetchExperiences(this.props.filter);
-      this.setState({
-        experiences,
-      });
-    }
+class DataProvider extends React.Component<
+  {},
+  { experiences: Experience[]; filter?: string }
+> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      experiences: [],
+    };
+  }
 
-    async componentDidMount() {
+  async fetchExperiences() {
+    const experiences = await fetchExperiences(this.state.filter);
+    this.setState({
+      experiences,
+    });
+  }
+
+  async componentDidMount() {
+    await this.fetchExperiences();
+  }
+
+  async componentDidUpdate(_prevProps: {}, prevState: { filter?: string }) {
+    if (this.state.filter !== prevState.filter) {
       await this.fetchExperiences();
     }
+  }
 
-    async componentDidUpdate(prevProps: { filter?: string }) {
-      if (this.props.filter !== prevProps.filter) {
-        await this.fetchExperiences();
-      }
-    }
-
-    render() {
-      return <Component experiences={this.state.experiences} />;
-    }
-  };
+  render() {
+    return (
+      <Provider
+        value={{
+          experiences: this.state.experiences,
+          onFilterChange: (filter: string) => {
+            this.setState({ filter });
+          },
+        }}
+      >
+        {this.props.children}
+      </Provider>
+    );
+  }
 }
 
 interface State {
   detailsShowedExperienceId?: string;
 }
 
-class FilteredExperienceList extends React.Component<
-  { experiences: Experience[] },
-  State
-> {
-  constructor(props: { experiences: Experience[] }) {
+class FilteredExperienceList extends React.Component<{}, State> {
+  constructor(props: {}) {
     super(props);
     this.state = {};
   }
   render() {
     return (
       <div className={styles['list-container']}>
-        {this.props.experiences.map(experience => (
-          <ExperienceCard
-            experience={experience}
-            key={experience.id}
-            showDetails={this.state.detailsShowedExperienceId === experience.id}
-            onClick={() => {
-              this.setState({
-                detailsShowedExperienceId:
-                  this.state.detailsShowedExperienceId !== experience.id
-                    ? experience.id
-                    : undefined,
-              });
-            }}
-          />
-        ))}
+        <Consumer>
+          {(context: DataProviderContext) =>
+            context.experiences.map(experience => (
+              <ExperienceCard
+                experience={experience}
+                key={experience.id}
+                showDetails={
+                  this.state.detailsShowedExperienceId === experience.id
+                }
+                onClick={() => {
+                  this.setState({
+                    detailsShowedExperienceId:
+                      this.state.detailsShowedExperienceId !== experience.id
+                        ? experience.id
+                        : undefined,
+                  });
+                }}
+              />
+            ))
+          }
+        </Consumer>
       </div>
     );
   }
@@ -140,5 +165,5 @@ const ExperienceSummary = ({
     <p className={styles['more-about']}>More about</p>
   </>
 );
-const ConnectedList = connectDataProvider(FilteredExperienceList);
+
 export default ExperienceList;
